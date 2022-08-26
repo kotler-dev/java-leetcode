f4aa6c617d54c86a6b93055b3f1be1964b170d46
Anton Kotler <kotler.developer@gmail.com>
2022-08-27 01:32:53 +0300

    ValidationBracketsTest

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 [9. Palindrome Number](https://github.com/kotler-dev/java-leetcode/blob/master/src/test/java/dev/kotler/problemset/easy/task100/PalindromeNumberTest.java)  
 [13. Roman to Integer](https://github.com/kotler-dev/java-leetcode/blob/master/src/test/java/dev/kotler/problemset/easy/task100/RomanToIntegerTest.java)  
 [14. Longest Common Prefix](https://github.com/kotler-dev/java-leetcode/blob/master/src/test/java/dev/kotler/problemset/easy/task100/LongestCommonPrefixTest.java)  
+[20. Valid Parentheses](https://github.com/kotler-dev/java-leetcode/blob/master/src/test/java/dev/kotler/problemset/easy/task100/ValidationBracketsTest.java)  
 [21. Merge Two Sorted Lists](https://github.com/kotler-dev/java-leetcode/blob/master/src/test/java/dev/kotler/problemset/easy/task100/MergeTwoSortedListsTest.java)  
 [26. Remove Duplicates from Sorted Array](https://github.com/kotler-dev/java-leetcode/blob/master/src/test/java/dev/kotler/problemset/easy/task100/RemoveDuplicatesFromSortedArrayTest.java)  
 [27. Remove Element](https://github.com/kotler-dev/java-leetcode/blob/master/src/test/java/dev/kotler/problemset/easy/task100/RemoveElementTest.java)  
---
+++ src/test/java/dev/kotler/problemset/easy/task100/ValidationBracketsTest.java
@@ -0,0 +1,88 @@
+package dev.kotler.problemset.easy.task100;
+
+/*
+
+https://leetcode.com/problems/valid-parentheses/
+
+20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+*/
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ValidationBracketsTest {
+    private static Stream<Arguments> generateValueTest() {
+        return Stream.of(
+                Arguments.of(true, "()"),
+                Arguments.of(true, "()[]{}"),
+                Arguments.of(true, "([])"),
+                Arguments.of(true, "([][])"),
+                Arguments.of(true, "([{}])"),
+                Arguments.of(false, "([)]"),
+                Arguments.of(false, "([)"),
+                Arguments.of(false, "(]"),
+                Arguments.of(false, "}{}"),
+                Arguments.of(false, "([{]})")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateValueTest")
+    void isNotValidTest(Boolean tf, String brackets) {
+        assertEquals(tf, isValid(brackets));
+    }
+
+    public static boolean isValid(String input) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> brackets = new HashMap<>();
+        brackets.put(']', '[');
+        brackets.put('}', '{');
+        brackets.put(')', '(');
+
+        for (char character : input.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(character);
+            } else {
+                Character peek = stack.peek();
+                if (brackets.get(character) == peek) {
+                    stack.pop();
+                } else {
+                    stack.push(character);
+                }
+            }
+        }
+
+        System.out.println(stack + " -> " + stack.empty());
+        return stack.empty();
+    }
+}
